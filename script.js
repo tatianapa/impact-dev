a0d340443b63996e9b5ed362f0758e9c867e98b1
Tatiana <tatiana.pavlov1991@gmail.com>
2023-07-26 12:30:11 +0300

    comment  adeed

--- script.js
+++ script.js
@@ -69,6 +69,9 @@ $(document).ready(function () {
 
     validator.checkAll();
     validator.reload();
+
+
+    //DANIEL ----- my functions below
     function toggleSecondList() {
       const firstListOptions = document.querySelectorAll('input[name="additional-deposits"]');
       const secondListContainer = document.getElementById('expected-amount-container');
